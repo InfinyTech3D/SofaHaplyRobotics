ab57d2b53216b303c8993c1b42bd7525af9407ec
erik pernod <erik.pernod@gmail.com>
2025-10-31 12:31:16 +0100

    [CMake] Add external lib for new WebSocket communication API. Integration not working yet (#6)
    * [backup] logs
    
    * backup notes
    
    * [CMake] Add external lib for websocket new version

---
+++ external/json/README.md
@@ -0,0 +1,5 @@
+# nlohmann json
+
+1. [GIT URL](https://github.com/nlohmann/json.git)
+2. [Release URL](https://github.com/nlohmann/json/releases/tag/v3.11.3)
+3. [Doc URL](https://github.com/nlohmann/json/tree/v3.11.3)
---
+++ external/libhv.h
@@ -0,0 +1,11 @@
+/* -*- mode: c++ -*-
+ * Copyright 2024 Haply Robotics Inc. All rights reserved.
+ */
+
+#ifdef __GNUC__
+    // To avoid gcc triggering warnings on third party library.
+    #pragma GCC system_header
+#endif
+
+#include <hv/WebSocketClient.h>
+#include <hv/requests.h>
